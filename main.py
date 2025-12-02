f773209f6d109eac9f5f14c818fc31ccb9f2c230
Joaquin Moreno <khas.mithrilwind@gmail.com>
2025-12-02 11:27:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
 from src.sakila_ETL import run_etl
 
 if __name__ == "__main__":
-    # Ejecuta  sakila_ETL completo: extrae -> transforma -> exporta CSV y xlsx para EXCEL
+    # Ejecuta sakila_ETL completo: extrae -> transforma -> exporta CSV y xlsx para EXCEL
     run_etl()
